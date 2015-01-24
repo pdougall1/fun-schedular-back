486de02db2a394430541878110d389080ca04e31
Patrick <pdougall1@gmail.com>
2015-01-24 15:28:58 -0500

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   def self.authenticate(email, password)
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_presence_of :name
+  validates_uniqueness_of :email
   	user = find_by(email: email)
   	user if user.password == password
   end
